32a87244d8f5687febd8f6540a9fb9e25722b6ff
Leen-odeh12 <123558998+Leen-odeh12@users.noreply.github.com>
2023-10-02 10:55:27 +0300

    Update answer.js

--- test/Q-Js/answer.js
+++ test/Q-Js/answer.js
@@ -1,23 +1,23 @@
 function findMissingNumber(nums) {
-  const n = nums.length;
-  let expectedSum = (n * (n + 1)) / 2;
-  let actualSum = 0;
+  const maxNumber = Math.max(...nums);
+  const minNumber = Math.min(...nums);
 
-  for (const num of nums) {
-    actualSum += num;
+  for (let i = minNumber; i <= maxNumber; i++) {
+    if (!nums.includes(i)) {
+      return i;
+    }
   }
 
-  return expectedSum - actualSum;
+  return maxNumber + 1;
 }
 
-
+// Examples
 const nums1 = [3, 0, 1];
 console.log(findMissingNumber(nums1)); // Output: 2
-
 
 const nums2 = [9, 6, 4, 2, 3, 5, 7, 0, 1];
 console.log(findMissingNumber(nums2)); // Output: 8
 
-
 const nums3 = [0, 1];
 console.log(findMissingNumber(nums3)); // Output: 2
+
