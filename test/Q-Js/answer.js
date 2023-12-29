235062eea4c7815caeacfa9918529ea1683b1d8a
leen <leenodeh287@gmail.com>
2023-12-29 11:27:02 +0300

    solve-Task

--- test/Q-Js/answer.js
+++ test/Q-Js/answer.js
@@ -1,23 +1,10 @@
-function findMissingNumber(nums) {
-  const maxNumber = Math.max(...nums);
-  const minNumber = Math.min(...nums);
 
-  for (let i = minNumber; i <= maxNumber; i++) {
-    if (!nums.includes(i)) {
-      return i;
-    }
+var missingNumber = function(nums) {
+  const res = new Array(nums.length+1).fill(-1);
+  for(const num of nums) {
+      res[num] = num;
   }
+  return res.indexOf(-1);
+};
 
-  return maxNumber + 1;
-}
-
-// Examples
-const nums1 = [3, 0, 1];
-console.log(findMissingNumber(nums1)); // Output: 2
-
-const nums2 = [9, 6, 4, 2, 3, 5, 7, 0, 1];
-console.log(findMissingNumber(nums2)); // Output: 8
-
-const nums3 = [0, 1];
-console.log(findMissingNumber(nums3)); // Output: 2
-
+// o(n)
